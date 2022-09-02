596c289e522242bdbbfbbd33151f0ec6161966a9
Colm O'Connor <colm.oconnor.github@gmail.com>
2022-09-02 21:28:47 +0000

    TOOLING : Fixed key.sh

--- key.sh
+++ key.sh
@@ -2,6 +2,15 @@
 set -e
 PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+hitchrun() {
+    podman run --privileged -it --rm \
+        -v $PROJECT_DIR:/src \
+        -v hitchrunpy-hitch-container:/gen \
+        --workdir /src \
+        hitchrunpy-hitch \
+        $1
+}
+
 case "$1" in
     "clean")
         if podman volume exists hitchrunpy-hitch-container; then
@@ -19,14 +28,14 @@ case "$1" in
         podman build -f hitch/Dockerfile-hitch -t hitchrunpy-hitch $PROJECT_DIR
         ;;
     "bash")
-        podman run --privileged -it --rm -v $PROJECT_DIR:/src -v hitchrunpy-hitch-container:/gen --workdir /src hitchrunpy-hitch bash
+        hitchrun "bash"
         ;;
     "--help")
         echo "Commands:"
         echo "./run.sh make     - build docker containers."
         ;;
     *)
-        podman run --privileged -it --rm -v $PROJECT_DIR:/src -v hitchrunpy-hitch-container:/gen --workdir /src hitchrunpy-hitch /venv/bin/python hitch/key.py $1 $2 $3 $4 $5 $6 $7 $8 $9
+        hitchrun "/venv/bin/python hitch/key.py $1 $2 $3 $4 $5 $6 $7 $8 $9"
         ;; 
 esac
 
